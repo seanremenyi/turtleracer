912e54aada466887dfa635c8735e25f819733631
Sean Remenyi <sean.remenyi@gmail.com>
2021-06-03 21:00:34 +1000

    turtle racer done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# turtleracer" 
+100 days of code - Python
+
+Day 19 - Turtle Racer
+
+This is a game where 6 turtles race from left to right. You can bet on the turtles as well based on their color.
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Turtle, Screen
-
+import random
+is_race_on = False
 
 screen = Screen()
 screen.setup(width=500, height=400)
@@ -7,12 +8,31 @@ user_bet = screen.textinput(title="Place your bets!", prompt="Choose a color of 
 
 colors = ["red", "blue", "green", "yellow", "orange","purple"]
 y_positions = [-80, -50, -20, 10, 40, 70]
+all_turtles = []
+
+
 for turtle_index in range(0, 6):
-    tim = Turtle(shape="turtle", )
-    tim.color(colors[turtle_index])
-    tim.penup()
-    tim.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle = Turtle(shape="turtle", )
+    new_turtle.color(colors[turtle_index])
+    new_turtle.penup()
+    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    all_turtles.append(new_turtle)
 
+if user_bet:
+    is_race_on = True
 
+while is_race_on:
+    for turtle in all_turtles:
+        if turtle.xcor() >230:
+            is_race_on = False
+            winning_turtle = turtle.pencolor()
+            if user_bet == winning_turtle:
+                print(f"You Won! the winning turtle was {winning_turtle}")
+            else:
+                print(f"You lost! The winning turtle was {winning_turtle}")
+
+        rand_distance = random.randint(0,10)
+        turtle.forward(rand_distance)
+screen.clear()
 
 screen.exitonclick()
